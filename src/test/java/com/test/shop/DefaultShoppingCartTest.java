f5179c3eafe5a17ed93c778e4a482740e32d55de
tarun.palisetty2 <tarun.palisetty@digital.hmrc.gov.uk>
2016-12-06 21:33:32 +0000

    Added junit shopping cart test

---
+++ src/test/java/com/test/shop/DefaultShoppingCartTest.java
@@ -0,0 +1,26 @@
+package com.test.shop;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DefaultShoppingCartTest {
+	
+	@Before
+	public void setUp(){
+		
+	}
+	
+	@After
+	public void tearDown(){
+		
+	}
+
+	@Test
+	public void testAddProduct() {
+		fail("Not yet implemented");
+	}
+
+}
